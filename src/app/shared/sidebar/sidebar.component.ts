34760a1b31e837b4dcdb8b0ac46a4736ccb89271
SardarBadar <dataentrybadar@gmail.com>
2022-12-17 11:41:50 +0500

    added image support

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "src/app/services/firebase/auth.service";
 import { environment } from "src/environments/environment";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { AngularFireStorage } from "@angular/fire/compat/storage";
 // import { AuthService } from "src/app/services/firebase/user.service";
 
 @Component({
@@ -9,11 +11,24 @@ import { environment } from "src/environments/environment";
   styleUrls: ["./sidebar.component.css"],
 })
 export class SidebarComponent {
-  public image = environment.baseUrl + "/assets/images/dp.png";
-  constructor(public auth: AuthService) {
-    this.image = this.auth.user?.photoURL
-      ? this.auth.user?.photoURL
+  public image: string | SafeResourceUrl =
+    environment.baseUrl + "/assets/images/dp.png";
+  constructor(
+    public auth: AuthService,
+    private sanitize: DomSanitizer,
+    private st: AngularFireStorage
+  ) {
+    const img = localStorage.getItem("img");
+
+    if (img) console.log("img", img);
+
+    this.image = img
+      ? this.sanitize.bypassSecurityTrustResourceUrl(img)
       : environment.baseUrl + "/assets/images/dp.png";
+  }
+  getImg(imgSrcRaw: string) {
+    let ref = this.st.ref(imgSrcRaw);
+    return ref.getDownloadURL();
   }
 
   ngOnInit(): void {}
